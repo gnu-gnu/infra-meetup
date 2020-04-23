e262e86b923068cd3181d1f5341c956e60268b94
geunwoo.shim <gflhsin@gmail.com>
2020-04-23 10:35:05 +0900

    initial

---
+++ src/main/java/com/gnu/inframeetup/InfraMeetupApplication.java
@@ -0,0 +1,25 @@
+package com.gnu.inframeetup;
+
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.config.NamingConvention;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InfraMeetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfraMeetupApplication.class, args);
+    }
+
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> metricConfig(){
+        return registry -> registry.config()
+                .namingConvention((name, type, unit) -> "infra-meetup."+name)
+                .commonTags("zone", "infra-1");
+    }
+
+}
---
+++ src/test/java/com/gnu/inframeetup/InfraMeetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnu.inframeetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraMeetupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
